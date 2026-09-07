6811bb6ea222c684a87967b4f4837d2291626e45
ansckddnjs17 <ansckddnjs170@naver.com>
2026-09-07 11:23:01 +0900

    68강

--- p-68-260818-back/src/main/java/com/back/p67260811/domain/member/entity/Member.java
+++ p-68-260818-back/src/main/java/com/back/p67260811/domain/member/entity/Member.java
@@ -28,6 +28,11 @@ public class Member extends BaseEntity {
         this.nickname = nickname;
     }
 
+    public Member(int id,String username){
+        this.setId(id);
+        this.username=username;
+    }
+
     public Member(String username, String password, String nickname, String apiKey) {
         this(username, password, nickname);
         this.apiKey = apiKey;
