15277c58fb75dfd818145750f10af1671dcd6c9f
ansckddnjs17 <ansckddnjs170@naver.com>
2026-09-07 12:25:13 +0900

    71

--- p-68-260818-back/src/main/java/com/back/p67260811/domain/member/controller/ApiV1MemberController.java
+++ p-68-260818-back/src/main/java/com/back/p67260811/domain/member/controller/ApiV1MemberController.java
@@ -117,13 +117,12 @@ public class ApiV1MemberController {
     public RsData<MemberDto> me() {
 
         Member actor = rq.getActor();
+        Member realActor = memberService.findById(actor.getId()).get();
 
         return new RsData(
                 "200-1",
                 "OK",
-                new MeResBody(
-                        new MemberDto(actor)
-                )
+                new MemberDto(realActor)
         );
     }
 
